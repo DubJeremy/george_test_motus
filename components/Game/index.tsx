e6c9bd94fcd633da1e02d4d7cf631e8b8aa3be4a
DubJeremy <dubois.jeremy33@gmail.com>
2023-12-07 16:30:03 +0100

    Gamepage

--- components/Game/index.tsx
+++ components/Game/index.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-import game from './game.module.scss';
+import styles from './game.module.scss';
 
 const Game = () => {
+	const [word, setWord] = useState('');
+	const [gameState, setGameState] = useState('start');
+
+	const getWord = () => {
+		axios
+			.get('https://trouve-mot.fr/api/random')
+			.then((res) => {
+				console.error('res1', res);
+
+				setWord(res.data[0].name);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
+
 	return (
-		<section>
+		<section className={styles.game}>
 			<h1>Motus</h1>
+			<button
+				className={styles.newWord}
+				onClick={() => setGameState('playing')}
+			>
+				start
+			</button>
+			{gameState === 'playing' ? (
+				<div className={styles.line}>
+					{' '}
+					{word.split('').map((letter, index) => (
+						<div className={styles.case} key={index}></div>
+					))}
+				</div>
+			) : null}
+			<button className={styles.newWord} onClick={() => getWord()}>
+				New word
+			</button>
+			<h4>{word}</h4>
 		</section>
 	);
 };
